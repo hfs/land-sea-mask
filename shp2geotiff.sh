a1a606a338d374cc9c8a7fe63926d7c20aca226a
Hermann Schwarting <hfs@gmx.de>
2014-12-08 21:15:54 +0100

    Generate GeoTIFFs with compressed 1-bit channel

--- shp2geotiff.sh
+++ shp2geotiff.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 BASEDIR=$(dirname "$0")
 
-# Use 254, because 255 is the default value for NaN
-gdal_rasterize -te -180.05 -90.05 180.05 90.05 -tr 0.1 0.1 -burn 254 -ot Byte -init 0 -at -i -a_srs "$(cat $BASEDIR/EPSG32663.wkt)" GSHHS_shp/i/GSHHS_i_L1.shp lsm_L1.geotiff
+gdal_rasterize -te -180.05 -90.05 180.05 90.05 -tr 0.1 0.1 -burn 1 -ot Byte -init 0 -at -i -a_srs "$(cat $BASEDIR/EPSG32663.wkt)" -co NBITS=1 -co COMPRESS=LZW GSHHS_shp/i/GSHHS_i_L1.shp lsm_L1.geotiff
 
 $BASEDIR/buffer.py GSHHS_shp/i/GSHHS_i_L2.shp GSHHS_i_L2_-buffer.shp -0.05
 
-gdal_rasterize -te -180.05 -90.05 180.05 90.05 -tr 0.1 0.1 -burn 254 -ot Byte -at -a_srs "$(cat $BASEDIR/EPSG32663.wkt)" GSHHS_i_L2_-buffer.shp lsm_L2.geotiff
+gdal_rasterize -te -180.05 -90.05 180.05 90.05 -tr 0.1 0.1 -burn 1 -ot Byte -at -a_srs "$(cat $BASEDIR/EPSG32663.wkt)" -co NBITS=1 -co COMPRESS=LZW GSHHS_i_L2_-buffer.shp lsm_L2.geotiff
 
-gdal_calc.py --calc 'A+B' -A lsm_L1.geotiff -B lsm_L2.geotiff --overwrite --outfile=lsm.geotiff
+gdal_calc.py --calc 'logical_or(A, B)' -A lsm_L1.geotiff -B lsm_L2.geotiff --creation-option='NBITS=1' --creation-option='COMPRESS=LZW' --overwrite --outfile=lsm.geotiff
